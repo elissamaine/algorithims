e84fb12e71dac0136111f11702d361021071c761
elissamaine <emaine97@yahoo.com>
2023-08-19 16:27:48 -0700

    m6 is-palindrome

--- M6/01-is-palindrome/Unsolved/is-palindrome.js
+++ M6/01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,14 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function(str) {
+  //set up variables for the string going forward and backwards
+  var string = str.toLowerCase();
+  var newString = string.split("").reverse().join("");
+
+  // check to see if the original sring and the new string are the same 
+  if (string === newString) {
+    return true;
+  } else {
+    return false;
+  }
+};
