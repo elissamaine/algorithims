1a3ade329131b3fbbc5247a18601ad2644a001af
elissamaine <emaine97@yahoo.com>
2023-08-22 23:55:33 -0700

    added pseudocode to m6 factorial algorithim

--- M6/02-factorial/Unsolved/factorial.js
+++ M6/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,9 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+  // set up a variable to hold the product of the factorial
+
+  //then set up a loop to going from 1 and ending at num
+    //multiply the product by the current number in the loop
+    
+};
